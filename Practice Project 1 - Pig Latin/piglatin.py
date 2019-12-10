0110840de56eec84faeb0eb6caadb6f34800d395
gvelkovski <55088075+gvelkovski@users.noreply.github.com>
2019-12-10 16:39:52 +0700

    Fixed indentation in order to print

--- Practice Project 1 - Pig Latin/piglatin.py
+++ Practice Project 1 - Pig Latin/piglatin.py
@@ -1,20 +1,18 @@
 """Pig Latin"""
 import sys
 
-"""Define which characters are vowels"""
-VOWELS = 'aeiouy'
+vowels = 'aeiouy'
 
 while True:
-    WORD = input("Type a word and get its Pig latin translation:")
+    word = input("Type a word and get its Pig latin translation: ")
 
-    if WORD[0] in VOWELS:
-        PIG_LATIN = WORD + 'way'
-
+    if word[0] in vowels:
+        pig_Latin = word + 'way'
     else:
-        PIG_LATIN = WORD[1:] + WORD[0] + 'ay'
-        print()
-        print("{}".format(PIG_LATIN), file=sys.stderr)
+        pig_Latin = word[1:] + word[0] + 'ay'
+    print()
+    print("{}".format(pig_Latin), file=sys.stderr)
 
-        TRY_AGAIN = input("\n\nTry again? (Press Enter else n to stop)\n")
-        if TRY_AGAIN.lower() == "n":
-            sys.exit()
+    try_again = input("\n\nTry again? (Press Enter else n to stop)\n")
+    if try_again.lower() == "n":
+        sys.exit()
