b1ed0704b457b508dc73769d276a819bc08574e4
gvelkovski <55088075+gvelkovski@users.noreply.github.com>
2019-12-11 09:56:25 +0700

    Palindroms load dictionary file

--- Chapter 2 - Palindrom Spells/palindroms.py
+++ Chapter 2 - Palindrom Spells/palindroms.py
@@ -1,3 +1,10 @@
-"""Load a text file as a list
+import load_dictionary
+word_list = load_dictionary.load('2of4brif.txt')
+pali_list = []
 
-Arguments
+for word in word_list:
+    if len(word) > 1 and word == word[::-1]:
+        pali_list.append(word)
+
+print("Number of palindromes found = {}\n".format(len(pali_list)))
+print(*pali_list, sep='\n')
